570ea0311b2712ac7b2899b8b9d8c86d36fcb8f8
Liam S. Crouch <liam@phoenixlan.no>
2022-10-01 12:38:49 +0200

    Send more info about seat if you are admin, for now

--- phoenixRest/mappers/seat.py
+++ phoenixRest/mappers/seat.py
@@ -1,11 +1,15 @@
 from phoenixRest.mappers.ticket import map_ticket_simple
 
+from phoenixRest.roles import HR_ADMIN, ADMIN, TICKET_ADMIN
+
 def map_seat_for_availability(seat, request):
+    is_admin = ADMIN in request.effective_principals or TICKET_ADMIN in request.effective_principals
     return {
         'uuid': str(seat.uuid),
         'number': seat.number,
         'is_reserved': seat.is_reserved,
         'taken': seat.ticket is not None,
+        'owner': (seat.ticket.owner if is_admin else None) if seat.ticket is not None else None,
         'ticket_id': seat.ticket.ticket_id if seat.ticket and (seat.ticket.owner.uuid == request.user.uuid or (seat.ticket.seater is not None and seat.ticket.seater.uuid == request.user.uuid)) else None
     }
 
