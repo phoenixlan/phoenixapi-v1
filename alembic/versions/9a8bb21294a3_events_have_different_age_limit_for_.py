4d2068d03f3c5bc7b6dc0a23b1a45b5f61c11150
Liam S. Crouch <liam@phoenixlan.no>
2023-01-08 21:13:56 +0900

    Fix

--- alembic/versions/9a8bb21294a3_events_have_different_age_limit_for_.py
+++ alembic/versions/9a8bb21294a3_events_have_different_age_limit_for_.py
@@ -21,7 +21,7 @@ def upgrade():
     op.create_unique_constraint(op.f('uq_application_crew_mapping_uuid'), 'application_crew_mapping', ['uuid'])
     op.add_column('event', sa.Column('crew_age_limit_inclusive', sa.Integer(), server_default='-1', nullable=False))
     op.alter_column('event', 'age_limit_inclusive', new_column_name='participant_age_limit_inclusive')
-    op.create_unique_constraint('_user_positionmapping_uic', 'user_positions', ['position_uuid', 'user_uuid'])
+    op.create_unique_constraint('_user_positionmapping_uic', 'user_positions', ['position_uuid', 'user_uuid', 'event_uuid'])
     # ### end Alembic commands ###
 
 
