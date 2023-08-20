94ada9983bfb011c84026e93afb05e2b257c9921
Liam S. Crouch <liam@phoenixlan.no>
2023-08-20 17:06:50 +0900

    debugging

--- install-deps-alpine.sh
+++ install-deps-alpine.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 set -e
 
-echo "Installing dependencies(Alpine"
-
 rm -r staticContent 
 rm -r docker
+
+echo "Installing dependencies(Alpine)"
 
 # Runtime dependencies
 apk add --no-cache postgresql-libs libpng libjpeg;
 # Build-time dependencies
 apk add --no-cache --virtual .build-deps build-base libffi-dev postgresql-dev jpeg-dev zlib-dev libpng-dev;
 
+echo "Installing pip dependencies"
 # Pip dependencies
 pip install -e . 
 # Hack??
 pip install psycopg2
 
+echo "Cleaning up..."
 mv alembic-docker.ini alembic.ini
 
 # Set up uploads directory
