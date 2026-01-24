857f9711b81a74a59574e529b33c5bbbfb49e471
Henrik Saura Engen <henriksen.engen@gmail.com>
2026-01-24 12:05:39 +0000

    Added edit_event code

--- phoenixRest/views/event/instance.py
+++ phoenixRest/views/event/instance.py
@@ -24,6 +24,8 @@ from phoenixRest.utils import validate
 from sqlalchemy import and_
 from sqlalchemy.orm import joinedload
 
+from datetime import datetime
+
 import logging
 log = logging.getLogger(__name__)
 
@@ -53,6 +55,9 @@ class EventInstanceResource(object):
 
             (Allow, CHIEF, 'applications_get'),
             (Allow, ADMIN, 'applications_get'),
+
+            (Allow, ADMIN, 'event_edit'),
+            (Allow, EVENT_ADMIN, 'event_edit'),
         ]
         return acl
 
@@ -124,3 +129,124 @@ def get_ticket_types(context, request):
     # Ticket types assigned to the event
     static_types = context.eventInstance.static_ticket_types
     return list(set(row_types+static_types))
+
+@view_config(name='edit', context=EventInstanceResource, request_method='PATCH', renderer='json', permission='event_edit')
+def edit_event(context, request):
+
+    error = list()
+
+    update_name = False
+    if 'name' in request.json_body:
+        if type(request.json_body['name']) != str:
+            error.append("Failed to update name, invalid type (not string)")
+        update_name = True
+
+    update_start_time = False
+    if 'start_time' in request.json_body:
+        try:
+            start_time = datetime.fromtimestamp(request.json_body['start_time'])
+            update_start_time = True
+        except:
+            error.append("Failed to update start_time, invalid format (cannot convert to datetime from integer)")
+
+    update_end_time = False
+    if 'end_time' in request.json_body:
+        try:
+            end_time = datetime.fromtimestamp(request.json_body['end_time'])
+            update_end_time = True
+        except:
+            error.append("Failed to update end_time, invalid format (cannot convert to datetime from integer)")
+
+    update_booking_time = False
+    if 'booking_time' in request.json_body:
+        try:
+            booking_time = datetime.fromtimestamp(request.json_body['booking_time'])
+            update_booking_time = True
+        except:
+            error.append("Failed to update booking_time, invalid format (cannot convert to datetime from integer)")
+
+    update_priority_seating_time_delta = False
+    if 'priority_seating_time_delta' in request.json_body:
+        if type(request.json_body['priority_seating_time_delta']) != int:
+            error.append("Failed to update priority_seating_time_delta, invalid type (not integer)")
+        update_priority_seating_time_delta = True
+
+    update_seating_time_delta = False
+    if 'seating_time_delta' in request.json_body:
+        if type(request.json_body['seating_time_delta']) != int:
+            error.append("Failed to update seating_time_delta, invalid type (not integer)")
+        update_seating_time_delta = True
+
+    update_max_participants = False
+    if 'max_participants' in request.json_body:
+        if type(request.json_body['max_participants']) != int:
+            error.append("Failed to update max_participants, invalid type (not integer)")
+        update_max_participants = True
+
+    update_participant_age_limit_inclusive = False
+    if 'participant_age_limit_inclusive' in request.json_body:
+        if type(request.json_body['participant_age_limit_inclusive']) != int:
+            error.append("Failed to update participant_age_limit_inclusive, invalid type (not integer)")
+        update_participant_age_limit_inclusive = True
+
+    update_crew_age_limit_inclusive = False
+    if 'crew_age_limit_inclusive' in request.json_body:
+        if type(request.json_body['crew_age_limit_inclusive']) != int:
+            error.append("Failed to update crew_age_limit_inclusive, invalid type (not integer)")
+        update_crew_age_limit_inclusive = True
+
+    update_theme = False
+    if 'theme' in request.json_body:
+        if request.json_body['theme'] is not None:
+            if type(request.json_body['theme']) != str:
+                error.append("Failed to update theme, invalid type (not string or None)")
+        update_theme = True
+
+    update_location_uuid = False
+    if 'location_uuid' in request.json_body:
+        if request.json_body['location_uuid'] is not None:
+            if type(request.json_body['location_uuid']) != str:
+                error.append("Failed to update location_uuid, invalid type (not string or None)")
+        update_location_uuid = True
+
+    update_seatmap_uuid = False
+    if 'seatmap_uuid' in request.json_body:
+        if request.json_body['seatmap_uuid'] is not None:
+            if type(request.json_body['seatmap_uuid']) != str:
+                error.append("Failed to update seatmap_uuid, invalid type (not string or None)")
+        update_seatmap_uuid = True
+
+    update_cancellation_reason = False
+    if 'cancellation_reason' in request.json_body:
+        if request.json_body['cancellation_reason'] is not None:
+            if type(request.json_body['cancellation_reason']) != str:
+                error.append("Failed to update cancellation_reason, invalid type (not string or None)")
+        update_cancellation_reason = True
+
+    if len(error) > 0:
+        request.response.status = 400
+        return {
+            'error': 'An error occured in one or more fields when attempting to update event information',
+            'data': error
+        }
+    
+    if update_name is True: context.eventInstance.name = request.json_body['name']
+    if update_start_time is True: context.eventInstance.start_time = start_time
+    if update_end_time is True: context.eventInstance.end_time = end_time
+    if update_booking_time is True: context.eventInstance.booking_time = booking_time
+    if update_priority_seating_time_delta is True: context.eventInstance.priority_seating_time_delta = request.json_body['priority_seating_time_delta']
+    if update_seating_time_delta is True: context.eventInstance.seating_time_delta = request.json_body['seating_time_delta']
+    if update_max_participants is True: context.eventInstance.max_participants = request.json_body['max_participants']
+    if update_participant_age_limit_inclusive is True: context.eventInstance.participant_age_limit_inclusive = request.json_body['participant_age_limit_inclusive']
+    if update_crew_age_limit_inclusive is True: context.eventInstance.crew_age_limit_inclusive = request.json_body['crew_age_limit_inclusive']
+    if update_theme is True: context.eventInstance.theme = request.json_body['theme']
+    if update_location_uuid is True: context.eventInstance.location_uuid = request.json_body['location_uuid']
+    if update_seatmap_uuid is True: context.eventInstance.seatmap_uuid = request.json_body['seatmap_uuid']
+    if update_cancellation_reason is True: context.eventInstance.cancellation_reason = request.json_body['cancellation_reason']
+    
+    return {
+        'info': 'Event information updated successfully',
+        'data': context.eventInstance
+    }
+    
+
